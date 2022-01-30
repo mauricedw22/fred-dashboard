38fca146b05df35ff39402dcdfb76aa7f9d3a987
Maurice WALKER <mauricedw22@gmail.com>
2022-01-30 18:28:47 -0500

    gold price and dates data

--- src/app/views/pages/dashboard/dashboard.component.ts
+++ src/app/views/pages/dashboard/dashboard.component.ts
@@ -72,12 +72,26 @@ export class DashboardComponent implements OnInit {
     // let txn_data: Object = {};
 
     async function prepare_array() {
-      // let arr1 = [];
 
-      const response = await fetch('https://api.stlouisfed.org/fred/series/observations?api_key=1160cbecd7a466e7d9b30234db259627&series_id=WPU081&file_type=json&observation_start=1985-06-01');
-      const body = await response.text();
-      const info = JSON.parse(body);
-      console.log(info)
+      // const response = await fetch('https://api.stlouisfed.org/fred/series/observations?api_key=1160cbecd7a466e7d9b30234db259627&series_id=WPU081&file_type=json&observation_start=1985-06-01');
+      // const body = await response.text();
+      // const info = JSON.parse(body);
+      // console.log(info)
+
+      const response2 = await fetch('../../assets/data/au-prices-1968-012922.json');
+      const body2 = await response2.text();
+      const info2 = JSON.parse(body2);
+      console.log(info2)
+
+      for(let i=0;i<info2.length;i++){
+
+        gold_array.push(info2[i].gold_price)
+        dates_array.push(info2[i].date)
+
+      }
+
+      console.log(gold_array)
+      console.log(dates_array)
 
       // for(let i=info.blocks-100;i<info.blocks;i++){
       //   const block_response = await fetch('https://api.whatsonchain.com/v1/bsv/main/block/height/' + String(i));
@@ -91,8 +105,8 @@ export class DashboardComponent implements OnInit {
 
       // }
 
-      console.log(dates_array);
-      console.log(gold_array);
+      // console.log(dates_array);
+      // console.log(gold_array);
     };
 
     prepare_array();
