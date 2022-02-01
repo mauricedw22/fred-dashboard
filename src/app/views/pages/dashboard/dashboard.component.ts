48fdb5f21a99f160a731527d5112c11fa091a522
Maurice WALKER <mauricedw22@gmail.com>
2022-01-31 21:43:43 -0500

    fixed getReservesChart function errors

--- src/app/views/pages/dashboard/dashboard.component.ts
+++ src/app/views/pages/dashboard/dashboard.component.ts
@@ -316,11 +316,11 @@ function getGoldPpiChartOptions(obj: any, yArr: Array<any>, xArr: Array<any>) {
 /**
  * Monthly sales chart options
  */
-function getReservesChartOptions(obj: any) {
+function getReservesChartOptions(obj: any, yArr: Array<any>, xArr: Array<any>) {
   return {
     series: [{
       name: 'Dollars',
-      data: [152,109,93,113,126,161,188,143,102,113,116,124]
+      data: yArr
     }],
     chart: {
       type: 'bar',
@@ -349,7 +349,7 @@ function getReservesChartOptions(obj: any) {
     },
     xaxis: {
       type: 'datetime',
-      categories: ['01/01/2022','02/01/2022','03/01/2022','04/01/2022','05/01/2022','06/01/2022','07/01/2022', '08/01/2022','09/01/2022','10/01/2022', '11/01/2022', '12/01/2022'],
+      categories: xArr, // ['01/01/2022','02/01/2022','03/01/2022','04/01/2022','05/01/2022','06/01/2022','07/01/2022', '08/01/2022','09/01/2022','10/01/2022', '11/01/2022', '12/01/2022'],
       axisBorder: {
         color: obj.gridBorder,
       },
